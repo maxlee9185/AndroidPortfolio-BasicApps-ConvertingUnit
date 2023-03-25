fb680329160a2ad0805190484eebe218b850d003
seonghyunLee <maxlee9185@naver.com>
2023-03-25 16:02:32 +0900

    saveInstanceState

--- app/src/main/java/android/portfolio/convertunitapp/MainActivity.kt
+++ app/src/main/java/android/portfolio/convertunitapp/MainActivity.kt
@@ -2,27 +2,29 @@ package android.portfolio.convertunitapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.portfolio.convertunitapp.databinding.ActivityMainBinding
 import android.util.Log
 import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
-
+    var cmToM = true
+    var inputNumber : Int = 0
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         var view = binding.root
         setContentView(view)
 
-        var cmToM = true
 
         val outpuEditTextView = binding.outputTextView
         val inputEditTextView = binding.inputEditText
         val outputUnitTextView = binding.outputUnitText
         val inputUnitTextView = binding.inputUnitText
         val swapImageButton = binding.swapButton
-        var inputNumber : Int = 0
+
         inputEditTextView.addTextChangedListener { text->
             inputNumber= if( text.isNullOrEmpty() ) { 0 }
             else {
@@ -53,5 +55,17 @@ class MainActivity : AppCompatActivity() {
                 outpuEditTextView.text= inputNumber.times(100).toString()
             }
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        outState.putBoolean("cmToM",cmToM)
+        super.onSaveInstanceState(outState, outPersistentState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        cmToM= savedInstanceState.getBoolean("cmToM")
+        binding.inputUnitText.text=if(cmToM) "cm" else "m"
+        binding.outputTextView.text=if(cmToM) "m" else "cm"
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
